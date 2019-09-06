39993d5de47ee1386e98e104cd164da4a399acc5
adi wielijarni <37235189+adiyani17a@users.noreply.github.com>
2019-09-06 20:06:49 +0700

    Update apiV1Controller.php

--- app/Http/Controllers/apiV1Controller.php
+++ app/Http/Controllers/apiV1Controller.php
@@ -24,6 +24,12 @@ class apiV1Controller extends Controller
 	public function getDataHome()
 	{
 		$data['hotDeal'] = $this->model->itinerary()->with(['itinerary_detail'])->where('hot_deals','Y')->where('active','true')->take(4)->get();
+
+		foreach ($data['hotDeal'] as $i => $d) {
+			$data['hotDeal'][$i]->itinerary_count = $d->itinerary_detail->count();
+			$data['hotDeal'][$i]->itinerary_hot_deal = $d->itinerary_detail->where('hot_deals','Y')->count();
+		}
+
 		$data['destination'] = $this->model->destination()->take(6)->get();
 
 		return Response::json($data);
