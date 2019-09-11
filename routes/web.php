888f80f2b1e16ca248e1a5d8ae1ef297380b79c7
adi wielijarni <37235189+adiyani17a@users.noreply.github.com>
2019-09-11 18:38:57 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -340,7 +340,7 @@ Route::group(['middleware' => 'check-token'], function () {
 
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization X-CSRF-TOKEN');
 
 
 
