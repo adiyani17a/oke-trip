d3b7780ee0cf15c9f33cce08e9cd11b8de094711
adi wielijarni <37235189+adiyani17a@users.noreply.github.com>
2019-09-13 18:03:21 +0700

    Update payment_history_d.php

--- app/payment_history_d.php
+++ app/payment_history_d.php
@@ -13,7 +13,7 @@ class payment_history_d extends Model
     const UPDATED_AT = 'updated_at';
     const CREATED_AT = 'created_at';
     protected $fillable = [
-							'id', 'dt', 'bank', 'nominal', 'account_name', 'image', 'date', 'created_at', 'updated_at'
+							'id', 'dt', 'bank', 'nominal', 'account_name','account_number', 'image', 'date', 'created_at', 'updated_at'
     					];
 
 }
