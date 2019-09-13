f9309616899ec694d139c7c9bd8a375c198d69b5
adi wielijarni <37235189+adiyani17a@users.noreply.github.com>
2019-09-14 02:56:35 +0700

    Update apiV1Controller.php

--- app/Http/Controllers/apiV1Controller.php
+++ app/Http/Controllers/apiV1Controller.php
@@ -191,7 +191,9 @@ class apiV1Controller extends Controller
 				$additional_counting = 1;
 				for ($i2=0; $i2 < count($room->additional[$i][$i1]); $i2++) { 
 					if (count($room->additional[$i][$i1][$i2]) != 0) {
-						dd($room->additional[$i][$i1]);
+						if ($i2 > 0) {
+							dd($additional_counting);
+						}
 						$data = array(
 									'id'				=> $id,
 									'id_booking_pax'	=> $i1+1,
