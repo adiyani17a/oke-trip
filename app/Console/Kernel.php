52c2cc221c4bc1fa321e6e3d924a9fded5c15048
adi wielijarni <37235189+adiyani17a@users.noreply.github.com>
2020-01-08 15:28:46 +0700

    asd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'App\Console\Commands\SendUserEmail'
+        '\App\Console\Commands\SendUserEmail'
     ];
 
     /**
