fac0080f871694107dbb3247a1821551fbe6c80c
adi wielijarni <37235189+adiyani17a@users.noreply.github.com>
2020-01-08 15:15:44 +0700

    asd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,9 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            DB::table('tes')->insert(['id'=>'1']);
-        })->everyMinute();
+        $schedule->command('notification:sendEmailphp')->everyMinute();
     }
 
     /**
