ec69d80da5e76f955919b17b434305f3b09597e0
adi wielijarni <37235189+adiyani17a@users.noreply.github.com>
2020-03-04 14:58:15 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
     {
         $token = generateToken();
         $groupMenu = $this->model->groupMenu()->get();
-        dd($groupMenu);
+        dd($groupMenu[0]);
         return view('home',compact('token','groupMenu'));
     }
 }
